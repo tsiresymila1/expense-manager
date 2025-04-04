bec4638d6f65f0d9dc9c1b4cf91bf52c2c6dda27
Tsiresy Milà <tsiresymila@gmail.com>
2025-04-04 17:17:00 +0300

    update(nextjs): use next js 15.3 canary

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,19 @@
+import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { authClient } from "./lib/auth-client";
 
 export async function middleware(request: NextRequest) {
-  const session = await authClient.getSession({
-    fetchOptions: {
-      headers: await headers(),
-    },
+  const data = await auth.api.getSession({
+    headers: await headers(),
   });
-  // const sessionCookie = getSessionCookie(request);
-  // console.log("sess::", sess);
-  // console.log("sessionCookie::", sessionCookie);
-  if (!session.data?.user) {
+  if (!data?.user) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
   return NextResponse.next({ request });
 }
 
 export const config = {
+  runtime: "nodejs",
   matcher: [
     "/((?!_next/static|api|auth|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
